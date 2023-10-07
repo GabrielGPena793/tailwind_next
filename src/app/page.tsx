37f5d7d7ca5c50dd3d0bd7cd87dad2c5c42505cb
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2023-10-07 20:00:25 -0300

    add title style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { SettingsTabs } from '@/components/SettingsTabs'
 export default function Home() {
   return (
     <>
-      <h1>Settings</h1>
+      <h1 className="text-3xl font-medium text-zinc-700">Settings</h1>
 
       <SettingsTabs />
     </>
