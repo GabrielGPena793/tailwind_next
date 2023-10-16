84180fd21a428aa5f243eb1135a40d6a2a812b71
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2023-10-16 11:58:31 -0300

    responsive menu

--- src/components/Form/FileInput/FIleList.tsx
+++ src/components/Form/FileInput/FIleList.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
-import { formatBytes } from '@/utils/Format-bytes'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { Button } from '@/components/Button'
 import { FIleItem } from './FIleItem'
 
 export function FIleList() {
